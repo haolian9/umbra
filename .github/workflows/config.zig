d3b2da2b663bf01eb2af2bc8646bc76796c1edbe
haoliang <haoliang0.1.2@gmail.com>
2022-06-21 13:29:12 +0800

    adds github workflows

---
+++ .github/workflows/config.zig
@@ -0,0 +1,5 @@
+// directories that contains video files.
+pub const roots = [_][]const u8{};
+
+// basename of trash bin, the dirname would be the mount-point dir
+pub const trash_dir = "umbra-trash";
