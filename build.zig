ea0d210ff5d6b1816b5ff6d518f4a2191a7a9afa
haoliang <haoliang0.1.2@gmail.com>
2022-06-21 14:59:12 +0800

    adds more targets in build.zig

--- build.zig
+++ build.zig
@@ -3,24 +3,55 @@ const Builder = std.build.Builder;
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
-    const output_dir = b.pathJoin(&.{ b.env_map.get("HOME").?, "bin" });
+    const output_dir = if (b.env_map.get("HOME")) |home| b.pathJoin(&.{ home, "bin" }) else null;
+
+    const strip = switch (mode) {
+        .Debug => false,
+        else => true,
+    };
+
+    const targets = [_]struct {
+        suffix: ?[]const u8,
+        target: std.zig.CrossTarget,
+    }{
+        .{
+            .suffix = "x86_64-linux-gnu",
+            .target = .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
+        },
+        .{
+            .suffix = "x86_64-linux-musl",
+            .target = .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
+        },
+        .{
+            // default
+            .suffix = null,
+            .target = .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
+        },
+    };
 
     {
-        const exe = b.addExecutable("umbra", "main.zig");
-        exe.setBuildMode(mode);
-        exe.setOutputDir(output_dir);
-        exe.strip = true;
-        exe.single_threaded = true;
-        exe.install();
+        inline for (targets) |ent| {
+            const name = if (ent.suffix) |suffix| "umbra-" ++ suffix else "umbra";
+            const exe = b.addExecutable(name, "main.zig");
+            exe.setBuildMode(mode);
+            exe.setTarget(ent.target);
+            if (output_dir) |dir| {
+                exe.setOutputDir(dir);
+            }
+            exe.strip = strip;
+            exe.single_threaded = true;
+            exe.install();
+        }
     }
 
     {
         const exe = b.addExecutable("lsmnts", "src/Mnts.zig");
         exe.setBuildMode(mode);
-        exe.setOutputDir(output_dir);
-        exe.strip = false;
+        if (output_dir) |dir| {
+            exe.setOutputDir(dir);
+        }
+        exe.strip = strip;
         exe.single_threaded = true;
         exe.install();
     }
-
 }
