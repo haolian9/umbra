22dcb27ed4b5ab90ea9ccc6adc0f60b0a97b7c57
haoliang <haoliang0.1.2@gmail.com>
2022-11-06 12:29:06 +0800

    no need for github action

--- .github/workflows/config.zig
+++
@@ -1,5 +0,0 @@
-// directories that contains video files.
-pub const roots = [_][]const u8{};
-
-// basename of trash bin, the dirname would be the mount-point dir
-pub const trash_dir = "umbra-trash";
--- build.zig
+++ build.zig
@@ -2,40 +2,11 @@ const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
     const mode = b.standardReleaseOptions();
+    const strip = mode != .Debug;
 
-    const strip = switch (mode) {
-        .Debug => false,
-        else => true,
-    };
-
-    const targets = [_]struct {
-        suffix: ?[]const u8,
-        target: std.zig.CrossTarget,
-    }{
-        .{
-            .suffix = "x86_64-linux-gnu",
-            .target = .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
-        },
-        .{
-            .suffix = "x86_64-linux-musl",
-            .target = .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
-        },
-        .{
-            // default
-            .suffix = null,
-            .target = .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
-        },
-    };
-
-    {
-        inline for (targets) |ent| {
-            const name = if (ent.suffix) |suffix| "umbra-" ++ suffix else "umbra";
-            const exe = b.addExecutable(name, "main.zig");
-            exe.setBuildMode(mode);
-            exe.setTarget(ent.target);
-            exe.strip = strip;
-            exe.single_threaded = true;
-            exe.install();
-        }
-    }
+    const exe = b.addExecutable("umbra", "main.zig");
+    exe.setBuildMode(mode);
+    exe.strip = strip;
+    exe.single_threaded = true;
+    exe.install();
 }
